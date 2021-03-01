c3ed4d16305298e31fb84c7091a48c731da51131
Yakov <kaktyzz-imba@mail.ru>
2021-03-01 12:20:11 +0300

    5

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,26 +4,26 @@ import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
-} from '../constants/productConstants'
+} from '../constants/productConstants' 
 
-const listProducts = () => async(dispatch) => {
-    try{
-        dispatch({type:PRODUCT_LIST_REQUEST})
+export const listProducts = () => async(dispatch) => {
+        try{
+            dispatch({type:PRODUCT_LIST_REQUEST})
 
-        const {data} = await axios.get('/api/products/')
+            const {data} = await axios.get('/api/products/')
 
-        dispatch({
-            type:PRODUCT_LIST_SUCCESS,
-            payload:data,
-        })
+            dispatch({
+                type:PRODUCT_LIST_SUCCESS,
+                payload:data,
+            })
 
 
-    }catch(error){
-        dispatch({
-            type:PRODUCT_LIST_FAIL,
-            payload:error.response && error.response.data.message
-                ? error.response.data.message
-                : error.message,
-        })
+        }catch(error){
+            dispatch({
+                type:PRODUCT_LIST_FAIL,
+                payload:error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+            })
+        }
     }
-}
