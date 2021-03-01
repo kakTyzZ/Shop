de10bf55243c4c147d6c0714a6bef2acc001036e
kakTyzZ <58885800+kakTyzZ@users.noreply.github.com>
2021-03-01 14:23:45 +0300

    6

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import {listProducts} from '../actions/productActions'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function HomeScreen() {
     const dispatch = useDispatch()
@@ -20,8 +22,8 @@ function HomeScreen() {
     return (
         <div>
             <h1>Latest products</h1>
-            { loading ? <h2>loading...</h2>
-                :error ? <h3>{error}</h3>
+            { loading ? <Loader />
+                :error ? <Message variant='danger'>{error}</Message>
                     :   <Row>
                         {products.map(product =>(
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
