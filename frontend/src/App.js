e04cdc554d483ef2eb5cc8d3b0f763bfffcd053b
Yakov <kaktyzz-imba@mail.ru>
2021-05-16 22:52:58 +0300

    may16

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
 
 
 function App() {
@@ -16,6 +20,10 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen}  />
+          <Route path='/register/' component={RegisterScreen}  />
+          <Route path='/profile/' component={ProfileScreen}  />
+          <Route path='/shipping' component={ShippingScreen}  />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
         </Container>
